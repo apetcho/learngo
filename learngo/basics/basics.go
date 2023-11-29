f2ac3c81a73ad6997d402300e367e8fa11e8452a
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-29 13:55:40 +0100

    Explore golang values

--- learngo/basics/basics.go
+++ learngo/basics/basics.go
@@ -5,3 +5,12 @@ import "fmt"
 func Hello() {
 	fmt.Println("Hello World!")
 }
+
+func Values(){
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
