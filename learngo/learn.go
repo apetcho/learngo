761383335f7d6ef82b5b60460ca50a809970a93c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2023-11-30 14:20:52 +0100

    Explore sorting by functions in golang

--- learngo/learn.go
+++ learngo/learn.go
@@ -59,4 +59,5 @@ func main() {
 	// level3.StatefuleGoroutines()
 
 	level4.Sorting()
+	level4.SortingByFunctions()
 }
--- learngo/level4/level4.go
+++ learngo/level4/level4.go
@@ -1,6 +1,7 @@
 package level4
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
@@ -23,4 +24,39 @@ func Sorting() {
 
 	s := slices.IsSorted(ints)
 	fmt.Println("Sorted: ", s)
+}
+
+// -*---------------------------*-
+// -*- (02) SortingByFunctions -*-
+// -*---------------------------*-
+func SortingByFunctions(){
+	fmt.Println()
+	fmt.Println("-*-----------------------------*-")
+	fmt.Println("-*- (02) Sorting by functions -*-")
+	fmt.Println("-*-----------------------------*-")
+
+	fruits := []string{"peach", "banana", "kiwi"}
+
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
+
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
+
+	type Person struct {
+		name string
+		age int
+	}
+
+	people := []Person{
+		{name: "Jax", age: 37},
+		{name: "TJ", age: 25},
+		{name: "Alex", age: 72},
+	}
+
+	slices.SortFunc(people, func(a, b Person) int{
+		return cmp.Compare(a.age, b.age)
+	})
+	fmt.Println(people)
 }
